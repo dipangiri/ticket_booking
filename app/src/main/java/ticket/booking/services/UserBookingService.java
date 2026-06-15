3927dfc40c27e5e901d09ef26cfd0ff7800ed792
Dipan Giri <dipangiri11@gmail.com>
2026-06-15 13:27:49 +0530

    added user booking service functions

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,28 +3,62 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class UserBookingService {
     private User user;
     private List<User> userList;
-    private static final String USERS_PATH = "app//localDb/users.json";
+    private final String USER_FILE_PATH = "app//localDb/users.json";
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    public UserBookingService(User user1) throws IOException {
-        this.user = user1;
-        File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        loadUserListFromFile();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
+
+    private void loadUserListFromFile() throws IOException {
+        userList = objectMapper.readValue(new File(USER_FILE_PATH), new TypeReference<List<User>>() {});
     }
 
     public Boolean loginUser(){
         Optional<User> foundUser = userList.stream().filter(user1 -> {
-            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1)
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword())
         }).findFirst();
         return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1) {
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+        } catch (IOException ex) {
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USER_FILE_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBookings() {
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        userFetched.ifPresent(User::printTickets);
+    }
+
+    public Boolean cancelBooking(String tickedId) {
+
     }
 }
